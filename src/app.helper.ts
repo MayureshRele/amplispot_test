370451acdaec9efe98480a82c37b7e2016615c13
MayureshRele <mayureshrele@gmail.com>
2023-11-06 09:47:27 +0530

    new changes

--- src/app.helper.ts
+++ src/app.helper.ts
@@ -2,6 +2,5 @@ import * as AWS from 'aws-sdk';
 const config = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
 };
 export const s3 = new AWS.S3(config);
